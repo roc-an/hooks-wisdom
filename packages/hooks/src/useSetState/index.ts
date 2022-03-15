02385044dc314a7a7e91cac72228102e25cf2322
anhongpeng <anhongpeng@meituan.com>
2022-03-16 07:25:18 +0800

    注解：useSetState

--- packages/hooks/src/useSetState/index.ts
+++ packages/hooks/src/useSetState/index.ts
@@ -5,11 +5,13 @@ export type SetState<S extends Record<string, any>> = <K extends keyof S>(
   state: Pick<S, K> | null | ((prevState: Readonly<S>) => Pick<S, K> | S | null),
 ) => void;
 
+// useSetState 支持函数式更新，通过对象解构赋值将计算后的新 state 扩充到旧 state 中
 const useSetState = <S extends Record<string, any>>(
   initialState: S | (() => S),
 ): [S, SetState<S>] => {
   const [state, setState] = useState<S>(initialState);
 
+  // 支持函数式更新，接收先前的 state，返回一个更新后的值
   const setMergeState = useCallback((patch) => {
     setState((prevState) => {
       const newState = isFunction(patch) ? patch(prevState) : patch;
