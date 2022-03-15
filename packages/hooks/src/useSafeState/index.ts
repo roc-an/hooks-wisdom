8c7c8c3c4fd80269b1aba11f443a4b182791b523
anhongpeng <anhongpeng@meituan.com>
2022-03-16 07:49:12 +0800

    注解：useSafeState

--- packages/hooks/src/useSafeState/index.ts
+++ packages/hooks/src/useSafeState/index.ts
@@ -2,6 +2,7 @@ import { useCallback, useState } from 'react';
 import type { Dispatch, SetStateAction } from 'react';
 import useUnmountedRef from '../useUnmountedRef';
 
+// 在 React.useState 的基础上，加了一步 useUnmountedRef 判断，如果组件已卸载，那么不更新 state 直接 return
 function useSafeState<S>(initialState: S | (() => S)): [S, Dispatch<SetStateAction<S>>];
 
 function useSafeState<S = undefined>(): [S | undefined, Dispatch<SetStateAction<S | undefined>>];
